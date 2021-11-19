f5cc7c901bda8f3b73d2e80a94db87a0d25b41e8
Katie Pundt <kzpundt@gmail.com>
2021-11-19 15:38:33 -0800

    refactor buildCurrencyString function and add working error handling

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import './css/styles.css';
 import CurrencyService from "./js/currency-service";
 
 function buildCurrencyString(response, usd, currency) {
-  console.log(currency);
   Object.keys(response.conversion_rates).forEach(function(key) {
-    if(key === currency.toUpperCase()) {
-      console.log(key);
-      console.log("currency found");
-      let convertedAmount = response.conversion_rates[key] * usd;
-      console.log(response.conversion_rates[key]);
-      console.log(convertedAmount);
-      $('#conversion-result').text(convertedAmount);
+    if (key === currency.toUpperCase()) {
+      let convertedAmount = (parseFloat(response.conversion_rates[key]) * usd).toFixed(2);
+      $('#conversion-result').text(`$${usd} converted to ${currency} is ${convertedAmount}.`);
+    } 
+    const currencyCodes = Object.keys(response.conversion_rates);
+    for (let i = 0; i < currencyCodes.length; i++) {
+      if (currency === currencyCodes[i]) {
+        return ("currency found");
+      }
     }
-    return `<p>Error!</p>`;
+    return $('#show-errors').text("Error! Please enter a valid currency code.");
   });
 }
 
@@ -30,12 +31,14 @@ function displayErrors(error) {
 
 $(document).ready(function(){
   $('#convert').click(function(){
+    let usd = $('#usd-amount').val();
+    let currency = $('#currency-code').val().toUpperCase();
     CurrencyService.getCurrency()
       .then(function(currencyResponse) {
         if (currencyResponse instanceof Error) {
           throw Error(`ExchangeRate-API error: ${currencyResponse.message}`);
         }
-        displayCurrency(currencyResponse, $('#usd-amount').val(), $('#currency-code').val().toUpperCase());
+        displayCurrency(currencyResponse, usd, currency);
       })
       .catch(function(error) {
         displayErrors(error.message);
