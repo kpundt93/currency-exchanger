eb05b6bfa8001cf6c4c0522dbfcc4870cf5e8c79
Katie Pundt <kzpundt@gmail.com>
2021-11-19 10:23:19 -0800

    add function to build currency string

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from "./js/currency-service";
 
+function buildCurrencyString(response, usd, currency) {
+  for (let i = 0; i < response.conversion_rates.length; i++) {
+    if (currency === response.conversion_rates[i]) {
+      return `<p>$${usd} USD is equivalent to ${usd * response.conversion_rates[i]} in ${currency}.</p>`;
+    }
+  }
+  return `<p>I'm sorry, we couldn't find an exchange rate for ${currency}.</p>`
+}
+
 function displayErrors(error) {
   $('#show-errors').text(`${error}`);
 }
@@ -12,7 +21,7 @@ $(document).ready(function(){
   $('#convert').click(function(){
     let usd = $('#usd-amount').val();
     let currency = $('#ex-currency').val();
-    CurrencyService.getCurrency(currency)
+    CurrencyService.getCurrency()
       .then(function(currencyResponse) {
         if (currencyResponse instanceof Error) {
           throw Error(`ExchangeRate-API error: ${currencyResponse.message}`);
