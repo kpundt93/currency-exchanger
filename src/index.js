2e776b74641b594fb1a74375dcb225e524425cc3
Katie Pundt <kzpundt@gmail.com>
2021-11-19 10:04:59 -0800

    add .click function and error handling

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from "./js/currency-service";
+
+function displayErrors(error) {
+  $('#show-errors').text(`${error}`);
+}
+
+$(document).ready(function(){
+  $('#convert').click(function(){
+    let usd = $('#usd-amount').val();
+    let currency = $('#ex-currency').val();
+    CurrencyService.getCurrency(currency)
+      .then(function(currencyResponse) {
+        if (currencyResponse instanceof Error) {
+          throw Error(`ExchangeRate-API error: ${currencyResponse.message}`);
+        }
+        displayCurrency(currencyResponse);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
+    console.log("usd-amount: " + usd);
+    console.log("exchange currency: " + currency);
+  });
+});
