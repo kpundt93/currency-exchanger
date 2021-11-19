74c979d69b63a626a0a73094b8f8bca4f65d8202
Katie Pundt <kzpundt@gmail.com>
2021-11-19 10:27:28 -0800

    update API call

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
   static getCurrency() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
     .then(function(response) {
       if(!response.ok) {
         throw Error(response.statusText);
